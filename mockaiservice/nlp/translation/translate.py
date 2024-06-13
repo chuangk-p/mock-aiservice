3abf2bb877d4b54a74c027cdbc12b73e9471efcc
chuangk-p <chuangk.piyawat@gmail.com>
2024-06-14 00:55:46 +0700

    update nlp package

--- mockaiservice/nlp/translation/translate.py
+++ mockaiservice/nlp/translation/translate.py
@@ -2,11 +2,11 @@ import requests
 import json
 from mockaiservice.setting.setting import get_api_key
 
-def translate(text:str, source:str, to:str, return_json:bool=False):
+def translate(text:str, src:str, to:str, return_json:bool=False):
     api_key = get_api_key()
     url = "https://api.aiforthai.in.th/xiaofan-zh-th"
     headers = {'Apikey':api_key, 'Content-Type':'application/json'}
-    data = json.dumps({"input": text, "src": source, "trg": to})
+    data = json.dumps({"input": text, "src": src, "trg": to})
     res = requests.post(url, data=data, headers=headers)
     if return_json == False:
         return res.json()['output']
