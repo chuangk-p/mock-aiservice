ea48d4a300400907e86c48644c884ef5c483957d
chuangk-p <chuangk.piyawat@gmail.com>
2024-06-14 15:14:41 +0700

    fixed requests error on tts module

--- mockaiservice/speech/tts/tts.py
+++ mockaiservice/speech/tts/tts.py
@@ -1,4 +1,5 @@
 import requests
+import time
 from mockaiservice.setting.setting import get_api_key
 
 def convert(text:str, path:str, speaker:int=0, phrase_break:int=0, audiovisual:int=0):
@@ -8,7 +9,8 @@ def convert(text:str, path:str, speaker:int=0, phrase_break:int=0, audiovisual:i
     url = 'https://api.aiforthai.in.th/vaja9/synth_audiovisual'
     data = {'input_text':text,'speaker': speaker, 
             'phrase_break':phrase_break, 'audiovisual':audiovisual}
-    
+            
+    time.sleep(1)
     response = requests.post(url, json=data, headers=headers)     
     resp = requests.get(response.json()['wav_url'],headers=headers)
     if resp.status_code == 200:
